e52933103748b2ce83e239819456d938c2fbde62
bmumz <brandimummery@gmail.com>
2020-12-31 20:17:42 -0500

    Refactoring.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react"
+import React from "react"
 import "normalize.css/normalize.css"
 import SEO from "../components/seo"
-import Insta from "../components/insta"
 import Navigation from "../components/navigation"
 import About from "../components/about"
 import Quote from "../components/quote"
@@ -12,44 +11,28 @@ import Footer from "../components/footer"
 
 import "../styles/styles.scss"
 
-class App extends Component {
-  state = {
-    show: false,
-  }
+const App = () => (
+  <div>
+    <SEO
+      title="Caswell Construction | Norfolk County"
+      description="Husband & Wife Team— offering services including Duradek installation, window and door installation, flooring, decks, drywall, residential painting, storage & organizing."
+      keywords="renovations Norfolk County, construction Norfolk County, renovations Delhi Ontario, construction Norfolk County, "
+    />
+    <Navigation />
 
-  showModal = () => {
-    this.setState({ show: true })
-  }
-
-  hideModal = () => {
-    this.setState({ show: false })
-  }
-
-  render() {
-    return (
-      <div>
-        <SEO
-          title="Caswell Construction | Norfolk County"
-          description="Husband & Wife Team— offering services including Duradek installation, window and door installation, flooring, decks, drywall, residential painting, storage & organizing."
-          keywords="renovations Norfolk County, construction Norfolk County, renovations Delhi Ontario, construction Norfolk County, "
-        />
-        <Navigation />
-
-        <div className="container">
-          <About />
-        </div>
-        <Quote />
-        <div className="container">
-          <Services />
-          <OurWork />
-        </div>
-        <Contact />
-        <div className="container">
-          <Footer />
-        </div>
-      </div>
-    )
-  }
-}
+    <div className="container">
+      <About />
+    </div>
+    <Quote />
+    <div className="container">
+      <Services />
+      <OurWork />
+    </div>
+    <Contact />
+    <div className="container">
+      <Footer />
+    </div>
+  </div>
+)
 
 export default App
